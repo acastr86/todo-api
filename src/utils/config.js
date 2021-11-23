94a5d1d0f3babfe6bee6658b43f02d8b8175f723
Adonis <acastr86@stu.southtexascollege.edu>
2021-11-22 21:02:45 -0600

    Added Prisma Migration

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,7 +8,8 @@ dotenv.config();
 const envSchema = joi.object().keys({
     NODE_ENV: joi.string().valid('development', 'production').required(),
     PORT: joi.number().positive().required(),
-    ORIGIN: joi.string().uri().required()
+    ORIGIN: joi.string().uri().required(),
+    DATABASE_URL: joi.string().uri().required(),
 }).unknown();
 
 const { value: env, error } = envSchema.prefs({ errors:{ label: 'key' }}).validate(process.env);
