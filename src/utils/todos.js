51703edba9a2eef03f6285c90a92befed1cc7bd2
Adonis <acastr86@stu.southtexascollege.edu>
2021-11-22 21:33:25 -0600

    Added DB

--- src/utils/todos.js
+++ src/utils/todos.js
@@ -1,45 +1,56 @@
-import { nanoid } from "nanoid";
 import joi from "joi";
+import { PrismaClient } from "@prisma/client";
 
 import logger from "./logger";
 
-const todos = [];
+const db = new PrismaClient();
+
 
 const todoSchema = joi.object({
     text: joi.string().min(10).required(),
     completed: joi.boolean().required()
 });
 
-const baseTodo = {
-    id: nanoid(),
-    text: 'Test To do item',
-    completed:  false,
-};
+const idSchema = joi.number().integer().positive().required();
 
-todos.push(baseTodo);
-
-export const getTodos = (completed = null) => {
+export const getTodos = async (completed = null) => {
 
     if(completed === null){
         logger.log.success("Getting all todos");
-        return todos;
+        return await db.todos.findMany();
     }
 
     logger.log.success('Getting by completion todos');
 
     const isCompleted = completed === 'true'? true: false;
 
-    return todos.filter((todo) => todo.completed === isCompleted);
+    return db.todos.findMany({
+        where:{
+            completed: isCompleted
+        }
+    });
 };
 
-export const getTodo = (id) => {
+export const getTodo = async (id) => {
+    logger.log.info(`Validating id: ${id}`);
+    const {error, value} = idSchema.validate(id);
+
+    if (error) {
+        logger.log.error(new Error(`Validation error: ${error.message}`));
+        return {error};
+    }
+
     logger.log.success(`Getting todo with id: ${id}`);
-    return todos.find((todo) => todo.id === id);
+    return await db.todos.findUnique({
+        where: {
+            id: value
+        }
+    });
 };
 
-export const addTodo = (todo) => {
-    logger.log.info(`Valiting ${todo} to add`);
-    const {error} = todoSchema.validate(todo);
+export const addTodo = async (todo) => {
+    logger.log.info(`Validating ${todo} to add`);
+    const {error, value} = todoSchema.validate(todo);
 
     if (error) {
         logger.log.error(new Error(`Validation error: ${error.message}`));
@@ -48,23 +59,41 @@ export const addTodo = (todo) => {
 
     logger.log.success(`Validated: ${todo}`); 
 
-    const newTodo = { id: nanoid(), ...todo};
-    todos.push(newTodo);
+    const newTodo = await db.todos.create({
+        data: {
+            ...value
+        }
+    });
+
     return {newTodo};
 };
 
-export const updateTodo = (id, todo) => {
+export const updateTodo = async (id, todo) => {
     logger.log.info(`Validating ${todo} for update`);
-    const {error} = todoSchema.validate(todo);
+    const {error: todoError, value: todoValue} = todoSchema.validate(todo);
 
-    if (error) {
-        logger.log.error(new Error(`Validation error: ${error.message}`));
-        return {error};
+    if (todoError) {
+        logger.log.error(new Error(`Validation error: ${todoError.message}`));
+        return {todoError};
     };
+    
+    logger.log.info(`Validating id: ${id}`);
+    const {error: idError, value: idValue} = idSchema.validate(id);
 
-    logger.log.success(`Validated: ${todo}`);
-    const todoIndex = todos.findIndex((t) => t.id === id);
-    todos[todoIndex] = {id, ...todo};
-    const updatedTodo = todos[todoIndex];
+    if (idError) {
+        logger.log.error(new Error(`Validation error: ${idError.message}`));
+        return {idError};
+    }
+
+    logger.log.success('Validated todo and ID ');
+
+    const updatedTodo = await db.todos.update({
+        where: {
+            id: idValue
+        },
+        data: {
+            ...todoValue
+        }
+    });
     return {updatedTodo};
 };
